5b1c0e26a21585b20a1c9295e27b6cc2ede7503d
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-20 13:28:14 -0500

    Update count_sentences.rb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else 
+      false
+    end 
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      false
+    end 
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      false
+    end 
 
   end
 
